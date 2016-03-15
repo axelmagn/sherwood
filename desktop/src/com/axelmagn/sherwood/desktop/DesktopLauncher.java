9b8e775f34c31bf5012f6721f8bf0e00a33a320b
Axel Magnuson <axelmagn@gmail.com>
2016-03-15 20:55:29 +0800

    Refactor package names

--- desktop/src/com/axelmagn/sherwood/desktop/DesktopLauncher.java
+++ desktop/src/com/axelmagn/sherwood/desktop/DesktopLauncher.java
@@ -1,12 +1,11 @@
-package com.mygdx.game.desktop;
+package com.axelmagn.sherwood.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		new LwjglApplication(new SherwoodGame(), config);
 	}
 }
